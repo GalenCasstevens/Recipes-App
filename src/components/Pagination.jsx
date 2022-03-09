eeccd9955261c7b2a33ff4ea8b596f0b2461ba13
Galen Casstevens <GCasstevens@noregon.com>
2022-03-09 11:41:27 -0500

    made pagination component display on the Ue UI

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import BSPagination from 'react-bootstrap/Pagination';
 
 function Pagination({ postsPerPage, totalPosts }) {
-	const pageNumbers = [];
+	const active = 1;
+	const items = [];
 
-	return <div></div>;
+	for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+		items.push(
+			<BSPagination.Item key={i} active={i === active}>
+				{i}
+			</BSPagination.Item>
+		);
+	}
+
+	return <BSPagination>{items}</BSPagination>;
 }
 
 export default Pagination;
--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { ListGroup, Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import RecipeData from '../data/RecipeData';
+import Pagination from './Pagination';
 
 function RecipeList() {
 	const [recipes, setRecipes] = useState(RecipeData.recipes);
@@ -24,36 +25,39 @@ function RecipeList() {
 	// };
 
 	return (
-		<ListGroup as="ul" className="recipe-list">
-			{recipes.map((recipe) => (
-				<ListGroup.Item as="li">
-					<Container fluid>
-						<Row>
-							<Col xs={2}>
-								<img src={recipe.image} alt="" />
-							</Col>
-							<Col xs={10}>
-								<div className="recipe-info">
-									<h5 className="recipe-title">
-										<Link to={`/recipe/${recipe.id}`}>
-											<strong>{recipe.title}</strong>
-										</Link>
-									</h5>
-									<p className="ingredients">
-										Ingredients:&nbsp;
-										{recipe.extendedIngredients.map((ingredient, index) => {
-											if (recipe.extendedIngredients.length - 1 !== index)
-												return <span>{ingredient.name}, </span>;
-											else return <span>{ingredient.name}</span>;
-										})}
-									</p>
-								</div>
-							</Col>
-						</Row>
-					</Container>
-				</ListGroup.Item>
-			))}
-		</ListGroup>
+		<div className="recipe-list-page">
+			<ListGroup as="ul" className="recipe-list">
+				{recipes.map((recipe) => (
+					<ListGroup.Item as="li">
+						<Container fluid>
+							<Row>
+								<Col xs={2}>
+									<img src={recipe.image} alt="" />
+								</Col>
+								<Col xs={10}>
+									<div className="recipe-info">
+										<h5 className="recipe-title">
+											<Link to={`/recipe/${recipe.id}`}>
+												<strong>{recipe.title}</strong>
+											</Link>
+										</h5>
+										<p className="ingredients">
+											Ingredients:&nbsp;
+											{recipe.extendedIngredients.map((ingredient, index) => {
+												if (recipe.extendedIngredients.length - 1 !== index)
+													return <span>{ingredient.name}, </span>;
+												else return <span>{ingredient.name}</span>;
+											})}
+										</p>
+									</div>
+								</Col>
+							</Row>
+						</Container>
+					</ListGroup.Item>
+				))}
+			</ListGroup>
+			<Pagination postsPerPage={10} totalPosts={100} />
+		</div>
 	);
 }
 
