3e907dbc0b970c991f313a85eda45bea54153268
Galen Casstevens <galencasstevens@gmail.com>
2022-07-23 17:20:30 -0400

    fixed bug calling API in recipes app

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -5,42 +5,40 @@ import RecipeItem from './RecipeItem';
 import Pagination from './Pagination';
 
 function RecipeList() {
-	const [recipes, setRecipes] = useState(RecipeData.recipes);
+	const [recipes, setRecipes] = useState([]);
 	const [recipesPerPage, setRecipesPerPage] = useState(6);
-	const [currentRecipes, setCurrentRecipes] = useState(
-		RecipeData.recipes.slice(0, recipesPerPage)
-	);
-	const [totalRecipes, setTotalRecipes] = useState(RecipeData.recipes.length);
+	const [totalRecipes, setTotalRecipes] = useState(100);
 	const [active, setActive] = useState(1);
 
+	const fetchRecipes = async () => {
+		const response = await fetch(
+			`${process.env.REACT_APP_SPOONACULAR_URL}/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
+		);
+
+		const data = await response.json();
+
+		console.log(data);
+
+		setRecipes(data.recipes);
+	};
+
+	useEffect(() => {
+		fetchRecipes();
+	}, [active]);
+
 	if (!recipes || recipes.length === 0) return <p>No Recipes Yet</p>;
-
-	// useEffect(() => {
-	// 	fetchRecipes();
-	// }, []);
-
-	// const fetchRecipes = async () => {
-	// 	const response = await fetch(
-	// 		`${process.env.REACT_APP_SPOONACULAR_URL}/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
-	// 	);
-
-	// 	const data = await response.json();
-
-	// 	setRecipes(data.recipes);
-	// 	setLoading(false);
-	// };
 
 	const paginate = (pageNumber) => {
 		const startRecipeIndex = (pageNumber - 1) * recipesPerPage;
 		const endRecipeIndex = pageNumber * recipesPerPage;
 		setActive(pageNumber);
-		setCurrentRecipes(recipes.slice(startRecipeIndex, endRecipeIndex));
+		setRecipes(recipes.slice(startRecipeIndex, endRecipeIndex));
 	};
 
 	return (
 		<div className="recipe-list-container">
 			<ListGroup as="ul" className="recipe-list">
-				{currentRecipes.map((recipe) => (
+				{recipes.map((recipe) => (
 					<RecipeItem recipe={recipe} />
 				))}
 			</ListGroup>
